b6d4f14e71989de31198e5291e16dbc632d103fb
Sol <sol@Sols-MacBook-Pro.local>
2016-10-22 19:34:19 -0400

    initial commit with server running - auth process not yet in place

--- server.js
+++ server.js
@@ -2,28 +2,40 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT                  = process.env.PORT || 8080;
+const ENV                   = process.env.ENV || "development";
+const express               = require("express");
+const bodyParser            = require("body-parser");
+const sass                  = require("node-sass-middleware");
+const app                   = express();
+const morgan                = require('morgan');
+const passport              = require("passport");
+const flash                 = require('connect-flash');
+const cookieParser          = require('cookie-parser');
+const expressSession        = require("express-session");
+const GitHubStrategy        = require("passport-github2").Strategy;
+const methodOverride        = require("method-override");
+const ejs                   = require("ejs");
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
-
+const GITHUB_CLIENT_ID      = process.env.GITHUB_CLIENT_ID
+const GITHUB_CLIENT_SECRET  = process.env.GITHUB_CLIENT_SECRET
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(err, user);
+    });
+  }
+));
 
-// Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+
+app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,16 +45,57 @@ app.use("/styles", sass({
   debug: true,
   outputStyle: 'expanded'
 }));
+app.use(cookieParser());
 app.use(express.static("public"));
+app.use(expressSession({ secret: 'wJWnfa2C7EjSSGpY', resave: false, saveUninitialized: false }));
+app.use(flash());
+app.use(methodOverride());
 
-// Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use(passport.initialize());
+app.use(passport.session({ secret: 'wJWnfa2C7EjSSGpY', resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
+//=== ROUTES ===================================================================
 // Home page
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("layout");
 });
 
+//GitHub OAuth
+app.get('/auth/github',
+passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback',
+passport.authenticate('github', { failureRedirect: '/login' }),
+function(req, res) {
+  // Successful authentication, redirect home.
+  res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("GitMarked listening on port " + PORT);
 });
