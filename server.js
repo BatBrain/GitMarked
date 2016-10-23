b2c74ffd7971169159c09219edf1ff0253ef6d38
Sol <sol@Sols-MBP.ccs>
2016-10-23 18:53:51 -0400

    Oauth working, login route working, yet to test requests after this point, user data part of req

--- server.js
+++ server.js
@@ -16,29 +16,54 @@ const expressSession        = require("express-session");
 const GitHubStrategy        = require("passport-github2").Strategy;
 const methodOverride        = require("method-override");
 const ejs                   = require("ejs");
+const partial               = require('express-partials');
+const chalk                 = require("chalk");
+
 
 const GITHUB_CLIENT_ID      = process.env.GITHUB_CLIENT_ID
 const GITHUB_CLIENT_SECRET  = process.env.GITHUB_CLIENT_SECRET
-// Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+
+//const usersRoutes = require("./routes/users");
+function log(text){console.log(chalk.black.bgYellow.bold(text))};
+
+
+//=== Strategy ===================================================================
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080"
+    callbackURL: "http://localhost:8080/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      return done(err, user);
+      log("GitHub returned something! Huzzah!");
+      // console.log("refreshToken: \n", refreshToken);
+      // console.log("accessToken: \n", accessToken);
+      // console.log("profile: \n", profile);
+      debugger
+      return done(null, profile);
     });
   }
 ));
 
 
+//=== MiddleWare ===================================================================
+
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
+app.use(partial());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -49,27 +74,36 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(expressSession({ secret: 'wJWnfa2C7EjSSGpY', resave: false, saveUninitialized: false }));
 app.use(flash());
-app.use(methodOverride());
 
 app.use(passport.initialize());
-app.use(passport.session({ secret: 'wJWnfa2C7EjSSGpY', resave: false, saveUninitialized: false }));
+//app.use(passport.session({ secret: 'wJWnfa2C7EjSSGpY', resave: false, saveUninitialized: false }));
 app.use(passport.session());
 
-//=== ROUTES ===================================================================
-// Home page
-app.get("/", (req, res) => {
-  res.render("layout");
-});
 
-//GitHub OAuth
+
+//=== GitHub OAuth ===================================================================
+
 app.get('/auth/github',
-passport.authenticate('github', { scope: [ 'user:email' ] }));
+  passport.authenticate('github', { scope: [ 'user', 'repo' ] }),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
 
 app.get('/auth/github/callback',
 passport.authenticate('github', { failureRedirect: '/login' }),
 function(req, res) {
+  //console.log("REQ HERE:", req)
+  //console.log("RES HERE:", res)
   // Successful authentication, redirect home.
   res.redirect('/');
+});
+
+
+//=== ROUTES ===================================================================
+
+app.get("/", (req, res) => {
+  res.render('index', { user: req.user });
 });
 
 app.get('/logout', function(req, res){
@@ -85,16 +119,20 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
+
+//=== MW Functions ===================================================================
+
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  if (req.isAuthenticated()) {
+    console.log("User Authenticated")
+    return next()
+  } else {
+    res.redirect('/login')
+  }
 }
 
+
+//=== Listen ===================================================================
 
 app.listen(PORT, () => {
   console.log("GitMarked listening on port " + PORT);
