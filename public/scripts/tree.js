a9b0f1ed02a7c52825e68b00ab0a3582c17faf8e
Sol <sol@Sols-MacBook-Pro.local>
2016-10-29 00:05:58 -0400

    Fully working tree, properly assigns folder icons, and question icon if file type does not have defined icon configured so that ajax query for repo is a variable in codemirror.ejs cleaned up stylesheet and script loading and moved all resources to local to ensure version control of all elements and speed up loading times.

--- public/scripts/tree.js
+++ public/scripts/tree.js
@@ -17,27 +17,36 @@ loadLESS("/stylesheets/file-icons")
 //examples url https://api.github.com/repos/DanBrooker/file-icons/git/trees/0f5d7379b0556c0d2e99110e2b6d9012922c9c31
 var treeMaster;
 $(() => {
-  console.log('fn');
+  //console.log('fn');
   $.ajax({
     method: "GET",
-    url: "https://api.github.com/repos/BatBrain/ar-excercises/git/trees/aef6baccc89b2a11545438510c379b6034aa189f?recursive=1",
+    url: repoURL,
   }).done((response) => {
           treeMaster = populateTree(response)
+          console.log("Treemaster:", treeMaster)
           $.jstree.defaults.core.data
-          $('#tree').jstree({
+          $('#tree')
+            .on('select_node.jstree', function (e, data) {
+              console.log("Delicious Data!:", data)
+              if (data.node.original.type == "blob") {
+                $.ajax({
+                  method: "GET",
+                  url: data.node.original.url,
+                  headers: {accept: "application/vnd.github.VERSION.raw"}
+                }).done((response) => { editor.setValue(response) });
+              } else {
+                console.log("Oh look, time to do nothing!")
+                return
+              }
+            })
+            .jstree({
             "core" : {
               "data" : treeMaster
-            },
-            "types": {
-              ".atom": {
-                  "icon" : false,
-                  "a_attr": {"class" : "icon-file-directory", "data-name":".atom"}
-                }
             },
             "plugins": ["theme", "types"]
           });
         })
-    console.log("Treemaster:", treeMaster)
+
 
     //$('#tree').jstree({ 'core' : { 'data' : JSON.stringify(treeMaster) } });
   })
@@ -46,34 +55,42 @@ function populateTree(res){
   var tree = res.tree.map((cv, index, arr) => {
     var pathArr = cv.path.split("/");
     var fileExt = cv.path.split(".")
-    console.log("File extention: ", fileExt)
+    //console.log("File extention: ", fileExt)
     var object = {
       "id" : cv.path,
       "text" : pathArr[pathArr.length - 1],
       "parent" : parentMaker(pathArr),
+      "type" : cv.type,
+      "url" : cv.url,
       "icon" : false,
-      "a_attr" : typeMaker(fileExt)
+      "a_attr" : typeMaker(fileExt, cv, pathArr)
     };
       return object;
     })
-  console.log("Tree:", tree)
+  //console.log("Tree:", tree)
   return tree
   }
 
-function typeMaker(fileExt){
-  console.log("typeMaker input: ", fileExt)
+function typeMaker(fileExt, cv, pathArr){
+  //console.log("typeMaker input: ", fileExt)
   if (fileExt.length > 1){
     var attr = {"class" : "icon-file-directory", "data-name": `.${fileExt[fileExt.length - 1]}`}
-    console.log("ATTR:", attr)
+    //console.log("ATTR:", attr)
     return attr
-  } else {
-    return "folder"
+  } else if (cv.type == "tree"){
+    var attr = {"class" : "icon-file-directory", "data-name": ".folder-open"}
+    //console.log("ATTR:", attr)
+    return attr
+  } else if (cv.type == "blob" && fileExt.length == 1) {
+    var attr = {"class" : "icon-file-directory", "data-name": ".file-text-o"}
+    //console.log("ATTR:", attr)
+    return attr
   }
 }
 
 function parentMaker(splitPath) {
   if (splitPath.length > 1) {
-    return splitPath.slice(0, -1).join('/');
+    return splitPath.slice(0, -1).join('/')
   } else {
     return "#"
   }
