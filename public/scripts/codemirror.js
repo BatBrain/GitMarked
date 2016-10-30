de8ad6b77854cb836985d879fb8d19305a6e9b34
Sol <sol@Sols-MacBook-Pro.local>
2016-10-29 21:32:14 -0400

    add code mirror highlighting function

--- public/scripts/codemirror.js
+++ public/scripts/codemirror.js
@@ -12,5 +12,22 @@ $(() => {
     url: "https://raw.githubusercontent.com/BatBrain/ar-excercises/711d67306bb5d11a1018e09fb33a2249c81355a8/exercises/exercise_5.rb",
   }).done((response) => {
     editor.setValue(response)
+    highlightLine([2, 4, 6, 8, 10])
   });
 });
+
+
+function highlightLine(lineNumber) {
+  lineNumber.forEach((cv, index) => {
+    var actualLineNumber = cv - 1;
+    editor.addLineClass(actualLineNumber, 'wrap', 'bg-warning');
+  })
+   //Line number is zero based index
+   //Select editor loaded in the DOM
+   //var myEditor = $("#body_EditorSource .CodeMirror");
+   //console.log(myEditor);
+   //console.log(myEditor[0].CodeMirror);
+   //var codeMirrorEditor = myEditor[0].CodeMirror;
+   //Set line css class
+
+}
